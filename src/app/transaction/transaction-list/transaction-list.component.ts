6304d8f59244d513d61571b6759bac39984640e6
PurityKomen <jepkorirpurity8@gmail.com>
2024-10-22 14:12:28 +0300

    added transaction api to the frontend

--- src/app/transaction/transaction-list/transaction-list.component.ts
+++ src/app/transaction/transaction-list/transaction-list.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AddTransactionsComponent } from '../add-transactions/add-transactions.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-transaction-list',
   standalone: true,
-  imports: [AddTransactionsComponent],
+  imports: [AddTransactionsComponent, HttpClientModule],
   templateUrl: './transaction-list.component.html',
-  styleUrl: './transaction-list.component.css'
+  styleUrl: './transaction-list.component.css',
 })
-export class TransactionListComponent {
-  title = 'Bank of Flatiron'
+export class TransactionListComponent implements OnInit {
+  title = 'Bank of Flatiron';
+  url: string = 'http://127.0.0.1:8000/transactions/';
+
+  constructor(private http: HttpClient) {}
+
+  getTransactions() {
+    this.http.get(this.url).subscribe({
+      next: (data) => {
+        console.log(data);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+
+  ngOnInit() {
+    this.getTransactions();
+  }
 }
